f2edd1e86d1c2a381eee3dcc9c2e4ed305258cda
Mikhail Karkachov <mikhail.karkachov@gmail.com>
2023-11-12 01:33:45 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API descripttion')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000, '0.0.0.0');
+}
+bootstrap();
